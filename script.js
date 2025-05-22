24bac1d631c1903adc13fbbfa2f27ed15672381b
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 14:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// video.addEventListener('DOMContentLoaded', () => {
+	
+// })
+video.addEventListener('timeupdate', () => {
+	const percent = (video.currentTime/video.duration)*100;
+	progressBar.style.width = `${percent}%`;
+});
+toggle.addEventListener('click', () => {
+	if(video.paused) {
+		video.play();
+		toggle.textContent=`❚❚`;
+	} else {
+		video.pause();
+		toggle.textContent = `►`;
+	}
+});
+
+ranges.forEach(range => {
+	range.addEventListener('input', () => {
+		video[range.name] = range.value;
+	});
+});
+
+skipButtons.forEach(skipBtn => {
+	skipBtn.addEventListener('click', () => {
+		video.currentTime += parseFloat(skipBtn.dataset.skip);
+	});
+});
